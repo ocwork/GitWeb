aaa7351563a8e87edab526478082e856e78d6343
ocwork <orkuncaprazwork@gmail.com>
2022-05-11 15:17:33 +0300

    Dockerfile

--- GitWebApp/Program.cs
+++ GitWebApp/Program.cs
@@ -20,6 +20,7 @@ namespace GitWebApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:1000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
